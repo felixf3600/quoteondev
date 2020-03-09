d9ffab4c11b2515056c79d4691e3bcadd0336656
felixf3600 <felixf3600@gmail.com>
2020-03-08 21:58:05 -0700

    fixed the search pages and search result pages

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,14 +10,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 	<div class="entry-content">				
+		<a href="<?php echo get_site_url();?>/<?php the_title();?>"><h2 id="quote-title"> <?php the_title();?> </h2> </a>
 			<li id="quote-content"> <?php the_content();?> </li>			
-			<li id="quote-title"> <?php the_title();?> 
-			<?php $meta = get_post_meta( get_the_ID(), '_qod_quote_source', TRUE );
-			$meta_url = get_post_meta( get_the_ID(), '_qod_quote_source_url', TRUE );
-			if ($meta):?>
-				<?php echo " , " . "<a id='quote-source' href='" . $meta_url . "'>" . $meta . "</a>"; ?>
-			</li>
-			<?php endif;?>	
 	</div><!-- .entry-content -->
 	</header><!-- .entry-header -->
 
